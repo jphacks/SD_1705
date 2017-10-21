9c92b76e0d4ebc0b2edc8bf075fa72382f3b7745
Takaaki_Okawa <katakana1818@gmail.com>
2017-10-21 14:50:03 +0900

    ひとまずログインしてDB登録からの情報表示→ログアウトまでの作った

--- app/controllers/login.py
+++ app/controllers/login.py
@@ -26,40 +26,45 @@ def before_request():
     if 'twitter_token' in session:
         with UserModel() as User:
             users = User.get_user_by_token(session['twitter_token'])
+            g.user = []
             for user in users:
-                g.user = user
+                g.user.append({
+                    'id':user.twitter_id,
+                    'name':user.user_name,
+                    'icon':user.icon_url
+                })
 
 
-@app.route('/login')
-def login_main():
-    return render_template('login.html')
-
-
-@app.route('/user/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
-    oauth_callback = url_for('user/authorized', _external=True)
-    params = {'oauth_callback':oauth_callback}
+    if request.method == 'POST':
+        oauth_callback = url_for('login.authorized', _external=True)
+        params = {'oauth_callback':oauth_callback}
 
-    r = twitter.get_raw_request_token(params=params)
-    data = parse_utf8_qsl(r.content)
+        r = twitter.get_raw_request_token(params=params)
+        data = parse_utf8_qsl(r.content)
 
-    session['twitter_oauth'] = (data['oauth_token'],data['oauth_token_secret'])
+        session['twitter_oauth'] = (data['oauth_token'],data['oauth_token_secret'])
 
-    return redirect(twitter.get_authorize_url(data['oauth_token'],**params))
+        return redirect(twitter.get_authorize_url(data['oauth_token'],**params))
+
+    print('login get')
+    login = g.user is not None
+    return render_template('login.html', login=login, user=g.user)
 
 
-@app.route('/user/logout')
+@app.route('/logout')
 def logout():
     session.pop('twitter_token',None)
-    return redirect(url_for('login_main'))
+    return redirect(url_for('login.login'))
 
 
-@app.route('/user/authorized')
+@app.route('/authorized')
 def authorized():
     request_token, request_token_secret = session.pop('twitter_oauth')
     if not 'oauth_token' in request.args:
         session['login_error'] = 'You did not authorize the request'
-        return redirect(url_for('login_main'))
+        return redirect(url_for('login.login'))
     try:
         creds = {
             'request_token':request_token,
@@ -68,17 +73,25 @@ def authorized():
         sess = twitter.get_auth_session(params=params,**creds)
     except Exception as e:
         session['login_error'] = 'There was a problem login into Twitter: '+str(e)
-        return redirect(url_for('login_main'))
+        return redirect(url_for('login.login'))
 
     verify = sess.get('account/verify_credentials.json',params={'format':'json'}).json()
 
-    with UserModel as User:
-        users = User.get_user_by_token(session['twitter_token'])
+    with UserModel() as User:
+        try:
+            users = User.get_user_by_token(request_token)
+            print("OK")
+            users = User.update_user_token(verify['id'], verify['screen_name'], request_token, request_token_secret)
+        except Exception as e:
+            print("NG")
+            users = User.create_user(verify['id'], verify['screen_name'], verify['profile_image_url'], request_token, request_token_secret)
+        g.user = []
         for user in users:
-            g.user = user
-        if user is None:
-            User.create_user(verify['id'], verify['screen_name'], verify['profile_image_url'], request_token, request_token_secret)
-        else:
-            User.update_user_token(verify['id'], verify['screen_name'], request_token)
+            g.user.append({
+                'id': user.twitter_id,
+                'name': user.user_name,
+                'icon': user.icon_url
+            })
+    session['twitter_token'] = request_token
 
-    return redirect(url_for('login_main'))
+    return redirect(url_for('login.login'))

ffb04fa28348542ec991cfa45c503482068f95de
YayaWatanabe <yaya.w.1026@gmail.com>
2017-10-22 07:02:32 +0900

    optionを追加

--- app/dammy_data.py
+++ app/dammy_data.py
@@ -8,4 +8,4 @@ for i in range(100):
   tmp1.create_fav(random.randint(2362304748, 2362304750), random.randint(1,100))
 
 for i in range(1000):
-  tmp2.create_restaurant(i + 1, 38.253834 + 0.001*i, 140.874074 + 0.001*i,"居酒屋", "a","adress","bud", "open", "park","https://www.hotpepper.jp/strJ001001485/")
+  tmp2.create_restaurant(i + 1, 38.253834 + 0.001*i, 140.874074 + 0.001*i,"居酒屋", "a", "adress", "bud", "open", "park","https://www.hotpepper.jp/strJ001001485/")

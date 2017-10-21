7711b80fbe25e10cc404da900d539d304df48a51
Takaaki_Okawa <katakana1818@gmail.com>
2017-10-21 14:49:03 +0900

    server.pyにsessionで使うためのSECRET_KEYかきました

--- app/server.py
+++ app/server.py
@@ -1,10 +1,14 @@
 from flask import Flask
-from controllers import *
+from controllers import login
+
+SECRET_KEY = '\xa2Q\x97\x85\x9f\xbc\x92\x1a\xdf\x85\xbe\xc1\xea{\x97\xb4|\xe83\x1b\xd0x\xca'
 
 app = Flask(__name__)
+app.secret_key = SECRET_KEY
 
 # ここにアプリを追加していく
 apps = [
+    login.app
 ]
 
 for a in apps:

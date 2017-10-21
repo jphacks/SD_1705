dfc0a657a9953c07cfa535d3f6ebd8e4bd4f0dc1
YayaWatanabe <yaya.w.1026@gmail.com>
2017-10-21 17:38:10 +0900

    toppage作った（データなし)

--- app/static/js/addform.js
+++ app/static/js/addform.js
@@ -1,11 +1,19 @@
-addInputForm()
+var click_count = 0;
+console.log(click_count);
 
-function addInputForm()
-{
-    var div_element = document.createElement("div");
-    div_element.innerHTML = '<form class="form-inline"><div class="form-group"><label class="control-label" for="focusedInput">経由地</label><input class="form-control" id="focusedInput" type="text"></div></form> ';
-    var parent_object = document.getElementById("piyo");
-    parent_object.appendChild(div_element);
+function addInputForm(){
+    if(click_count < 8){
+
+        var div_element = document.createElement("div");
+        div_element.innerHTML = '<div class="row"><div class="col-xs-3"><div class="form-group"><input class="form-control" placeholder="経由地を入力" size = "15"></div></div></div>';
+        var parent_object = document.getElementById("way");
+        parent_object.appendChild(div_element);
+        click_count++;
+        console.log(click_count);
+    }
+    else{
+        alert("経由値は8個までです");
+    }
 }
 
 

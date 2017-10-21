dec7af859a7f523b2fe9ce64e08496f55474ebd1
Yuki Nozaki <yuuki060127@gmail.com>
2017-10-21 09:53:35 +0900

    dbのテーブルクラス作成

---
+++ app/models/model_parent.py
@@ -0,0 +1,61 @@
+from sqlalchemy import *
+import sqlalchemy.sql
+import sqlalchemy.orm
+import sqlalchemy.ext.declarative
+Base = sqlalchemy.ext.declarative.declarative_base()
+url = 'sqlite:///db/db.db'
+engine = sqlalchemy.create_engine(url)
+Session = sqlalchemy.orm.sessionmaker(bind=engine)
+
+
+class User(Base):
+    '''
+    usersテーブルのカラム
+    '''
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    twitter_id = Column(Integer, unique=True, nullable=False)
+    user_name = Column(Text, nullable=False)
+    icon_url = Column(Text)
+    token = Column(Text, nullable=False)
+    secret = Column(Text, nullable=False)
+    created_at = Column(Date, server_default=sqlalchemy.sql.func.now(), nullable=False)
+    update_at = Column(Date, server_default=sqlalchemy.sql.func.now(), nullable=False)
+    delete_at = Column(sqlalchemy.Text, default=None)
+
+
+class Restaurant(Base):
+    '''
+    restaurantsテーブルのカラム
+    '''
+    id = Column(Integer, primary_key=True)
+    lat = Column(Numeric, nullable=False)
+    lng = Column(Numeric, nullable=False)
+    name = Column(Text, nullable=False)
+    address = Column(Text, nullable=False)
+    budget = Column(Text, nullable=False)
+    open = Column(Text, nullable=False)
+    parking = Column(Boolean, nullable=False)
+    url = Column(Text, nullable=False)
+    created_at = Column(Date, server_default=sqlalchemy.sql.func.now(), nullable=False)
+    update_at = Column(Date, server_default=sqlalchemy.sql.func.now(), nullable=False)
+    delete_at = Column(sqlalchemy.Text, default=None)
+
+
+class Favorites(Base):
+    '''
+    favoiteテーブルのカラム
+    '''
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer, nullable=False)
+    id_restaurant = Column(Integer, nullable=False)
+    created_at = Column(Date, server_default=sqlalchemy.sql.func.now(), nullable=False)
+    update_at = Column(Date, server_default=sqlalchemy.sql.func.now(), nullable=False)
+    delete_at = Column(sqlalchemy.Text, default=None)
+
+
+def create_tables():
+    '''
+    テーブルデータの作成
+    '''
+    Base.metadata.create_all(engine)

227eb649fee018d35936d4527891520c96fd25e4
YayaWatanabe <yaya.w.1026@gmail.com>
2017-10-21 18:22:28 +0900

    データの送信できるようにした

--- app/static/js/addform.js
+++ app/static/js/addform.js
@@ -3,9 +3,10 @@ console.log(click_count);
 
 function addInputForm(){
     if(click_count < 8){
-
+        var way = "way" + click_count;
+        console.log(way);
         var div_element = document.createElement("div");
-        div_element.innerHTML = '<div class="row"><div class="col-xs-3"><div class="form-group"><input class="form-control" placeholder="経由地を入力" size = "15"></div></div></div>';
+        div_element.innerHTML = '<div class="row"><div class="col-xs-3"><div class="form-group"><input class="form-control" name =' + way + ' value ="" placeholder="経由地を入力" size = "15"></div></div></div>';
         var parent_object = document.getElementById("way");
         parent_object.appendChild(div_element);
         click_count++;
